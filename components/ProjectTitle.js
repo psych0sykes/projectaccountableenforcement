12f3105c0777926fb74df6dc6b1daca525f73e7b
blake <bleak@bleak.local>
2020-06-15 18:49:52 -0600

    Cleared the next.js template. Added _app.js and global css. Added Section, Nav, and Title components.

---
+++ components/ProjectTitle.js
@@ -0,0 +1,19 @@
+export default function ProjectTitle() {
+
+    const style = {
+        margin: "0px",
+    }
+
+    return(
+        <div style={{display: "flex",alignItems: "center"}}>
+            <div>
+                <h2 style={{margin: "20px"}}>the</h2>
+            </div>
+            <div>
+                <h1 style={style}>Accountable</h1>
+                <h1 style={style}>Enforcement</h1>
+                <h2 style={style}>project</h2>
+            </div>
+        </div>
+    )
+}
